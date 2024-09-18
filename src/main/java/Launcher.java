20acc014c37c9ed764c0601439c607c1995d3cbc
saajidshaik02 <saajidshaik02@gmail.com>
2024-09-19 00:49:12 +0800

    Level 3 Completed: Implement Input and Output msgs

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
 
+/**
+ * A launcher class to workaround classpath issues.
+ */
 public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
 }
